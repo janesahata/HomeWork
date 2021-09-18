3d121113585c35d24affbb6846bd9d2eb282f7e5
Jane <90954066+janesahata@users.noreply.github.com>
2021-09-18 11:24:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("5+5*2")
-print (5+5)
+value_1 = int (input("value: "))
+value_2 = int (input("value: "))
+operator = input("operator (+\-\/\*): ")
+if operator == "+":
+    if value_1 and value_2:
+        print(int(value_1) + int(value_2))
+    else:
+        print('bad')
+elif operator == "-":
+    if value_1 and value_2:
+        print(int(value_1) - int(value_2))
+    else:
+        print('bad')
+if operator == "*":
+    if value_1 and value_2:
+        print(int(value_1) * int(value_2))
+    else:
+        print('bad')
+if operator == "/":
+    if value_1 and value_2:
+        print(int(value_1) / int(value_2))
+    else:
+        print('bad')
