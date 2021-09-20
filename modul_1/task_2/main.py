425b6a52ecdfcbaa10476f876c86cd0660382a60
janesahata <evgenija.anna8@gmail.com>
2021-09-20 13:37:58 +0300

    task_2

--- modul_1/task_2/main.py
+++ modul_1/task_2/main.py
@@ -1,4 +1,7 @@
-value_1 = float(input("monthly salary UAH: "))
-value_2 = float(input("monthly loan payment UAH: "))
-value_3 = float(input("utility bills UAH: "))
-print(value_1-value_2-value_3)
+try:
+  value_1 = float(input("monthly salary UAH: "))
+  value_2 = float(input("monthly loan payment UAH: "))
+  value_3 = float(input("utility bills UAH: "))
+  print(value_1-value_2-value_3)
+except Exception:
+  print("bad number")
