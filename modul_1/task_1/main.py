8572a4d9a1b24e1d3afe028abeb3f130b4a55d32
janesahata <evgenija.anna8@gmail.com>
2021-09-20 11:16:28 +0300

    task1

--- modul_1/task_1/main.py
+++ modul_1/task_1/main.py
@@ -1,2 +1,11 @@
+value_1 = int(input("enter value1: "))
+value_2 = int(input("enter value2: "))
+value_3 = int(input("enter value3: "))
 
-print("task 1")
+operator = input("enter operator ('+' or '*')")
+if operator == "+":
+  print(value_1 + value_2 + value_3)
+elif operator == "*":
+  print(value_1 * value_2 * value_3)
+else:
+  print("bad operator")
